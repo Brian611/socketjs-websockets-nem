810c4c8da85588432ecc70e860f36785baa1f426
Brian Pooe <bp@hhpty.com>
2018-09-21 10:26:19 +0200

    added nem-sdk sockets

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-var stomp = require('stompjs');
+// Load nem-browser library
+var nem = require("nem-sdk").default;
 const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({ port: 3002 });
@@ -12,29 +13,90 @@ wss.on('connection', function connection(ws) {
         }
         console.log('address: %s', message);
     });
-    console.log("==============================");
 
-    let stompClient = stomp.overWS('ws://50.3.87.123:7778/w/messages/websocket');
-    let addr = "";
-    stompClient.debug = undefined;
-    stompClient.connect({}, function (frame) {
-        stompClient.subscribe(`/unconfirmed/${addr}`,
-            function (data) {
-                var body = JSON.parse(data.body);
-                console.log("unconfirmed: ", body);
-                ws.send(JSON.stringify({ unconfirmed: body }));
-            });
-
-        stompClient.subscribe(`/transactions/${addr}`,
-            function (data) {
-                var body = JSON.parse(data.body);
-                console.log("confirmed: ", body);
-                ws.send(JSON.stringify({ confirmed: body }));
-            });
-    });
+    // Create an NIS endpoint object
+    var endpoint = nem.model.objects.create("endpoint")("http://50.3.87.123", nem.model.nodes.websocketPort);
+    // Create a connector object
+    var connector = nem.com.websockets.connector.create(endpoint, addr);
+    // Try to establish a connection
+    connect(connector, ws);
 
     ws.on('close', function close() {
         console.log(`disconnected : ${addr}`);
     });
-
 });
+
+
+// Connect using connector
+function connect(connector, ws) {
+    return connector.connect().then(function () {
+        // Set time
+        date = new Date();
+
+        // If we are here, we are connected
+        console.log(date.toLocaleString() + ': Connected to: ' + connector.endpoint.host);
+
+        // Show event
+        console.log(date.toLocaleString() + ': Subscribing to errors');
+
+        // Subscribe to errors channel
+        nem.com.websockets.subscribe.errors(connector, function (res) {
+            // Set time
+            date = new Date();
+            // Show event
+            console.log(date.toLocaleString() + ': Received error');
+            // Show data
+            console.log(date.toLocaleString() + ': ' + JSON.stringify(res));
+        });
+
+        // Show event
+        console.log(date.toLocaleString() + ': Subscribing to unconfirmed transactions of ' + connector.address);
+
+        // Subscribe to unconfirmed transactions channel
+        nem.com.websockets.subscribe.account.transactions.unconfirmed(connector, function (res) {
+            // Set time
+            date = new Date();
+            // Show event
+            console.log(date.toLocaleString() + ': Received unconfirmed transaction');
+            // Show data
+            console.log(date.toLocaleString() + ': ' + JSON.stringify(res));
+            ws.send(JSON.stringify({ unconfirmed: res }));
+        });
+
+        // Show event
+        console.log(date.toLocaleString() + ': Subscribing to confirmed transactions of ' + connector.address);
+
+        // Subscribe to confirmed transactions channel
+        nem.com.websockets.subscribe.account.transactions.confirmed(connector, function (res) {
+            // Set time
+            date = new Date();
+            // Show event
+            console.log(date.toLocaleString() + ': Received confirmed transaction');
+            // Show data
+            console.log(date.toLocaleString() + ': ' + JSON.stringify(res));
+        });
+
+    }, function (err) {
+        // Set time
+        date = new Date();
+        // Show event
+        console.log(date.toLocaleString() + ': An error occured');
+        // Show data
+        console.log(date.toLocaleString() + ': ' + JSON.stringify(err));
+        // Try to reconnect
+        reconnect();
+    });
+}
+
+function reconnect() {
+    // Replace endpoint object
+    endpoint = nem.model.objects.create("endpoint")("http://bob.nem.ninja", 7778);
+    // Replace connector
+    connector = nem.com.websockets.connector.create(endpoint, address);
+    // Set time
+    date = new Date();
+    // Show event
+    console.log(date.toLocaleString() + ': Trying to connect to: ' + endpoint.host);
+    // Try to establish a connection
+    connect(connector);
+}

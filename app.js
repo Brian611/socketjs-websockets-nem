0f5a784c57b9f5ab6363edb243ae4219fd4af7dc
Brian Pooe <bp@hhpty.com>
2018-09-21 10:51:10 +0200

    moved functions inside websockets wrapper

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 3002 });
 
 wss.on('connection', function connection(ws) {
-    let addr = ""; 
+    let addr = "";
     ws.on('message', function incoming(message) {
         let response = JSON.parse(message);
         if (response.eventType == "address") {
@@ -21,82 +21,81 @@ wss.on('connection', function connection(ws) {
     // Try to establish a connection
     connect(connector, ws);
 
+    // Connect using connector
+    function connect(connector, ws) {
+        return connector.connect().then(function () {
+            // Set time
+            date = new Date();
+
+            // If we are here, we are connected
+            console.log(date.toLocaleString() + ': Connected to: ' + connector.endpoint.host);
+
+            // Show event
+            console.log(date.toLocaleString() + ': Subscribing to errors');
+
+            // Subscribe to errors channel
+            nem.com.websockets.subscribe.errors(connector, function (res) {
+                // Set time
+                date = new Date();
+                // Show event
+                console.log(date.toLocaleString() + ': Received error');
+                // Show data
+                console.log(date.toLocaleString() + ': ' + JSON.stringify(res));
+            });
+
+            // Show event
+            console.log(date.toLocaleString() + ': Subscribing to unconfirmed transactions of ' + connector.address);
+
+            // Subscribe to unconfirmed transactions channel
+            nem.com.websockets.subscribe.account.transactions.unconfirmed(connector, function (res) {
+                // Set time
+                date = new Date();
+                // Show event
+                console.log(date.toLocaleString() + ': Received unconfirmed transaction');
+                // Show data
+                console.log(date.toLocaleString() + ': ' + JSON.stringify(res));
+                ws.send(JSON.stringify({ unconfirmed: res }));
+            });
+
+            // Show event
+            console.log(date.toLocaleString() + ': Subscribing to confirmed transactions of ' + connector.address);
+
+            // Subscribe to confirmed transactions channel
+            nem.com.websockets.subscribe.account.transactions.confirmed(connector, function (res) {
+                // Set time
+                date = new Date();
+                // Show event
+                console.log(date.toLocaleString() + ': Received confirmed transaction');
+                // Show data
+                console.log(date.toLocaleString() + ': ' + JSON.stringify(res));
+                ws.send(JSON.stringify({ confirmed: res }));
+            });
+
+        }, function (err) {
+            // Set time
+            date = new Date();
+            // Show event
+            console.log(date.toLocaleString() + ': An error occured');
+            // Show data
+            console.log(date.toLocaleString() + ': ' + JSON.stringify(err));
+            // Try to reconnect
+            reconnect();
+        });
+    }
+
+    function reconnect() {
+        // Replace endpoint object
+        endpoint = nem.model.objects.create("endpoint")("http://bob.nem.ninja", 7778);
+        // Replace connector
+        connector = nem.com.websockets.connector.create(endpoint, address);
+        // Set time
+        date = new Date();
+        // Show event
+        console.log(date.toLocaleString() + ': Trying to connect to: ' + endpoint.host);
+        // Try to establish a connection
+        connect(connector);
+    }
     ws.on('close', function close() {
         console.log(`disconnected : ${addr}`);
     });
 });
-
-
-// Connect using connector
-function connect(connector, ws) {
-    return connector.connect().then(function () {
-        // Set time
-        date = new Date();
-
-        // If we are here, we are connected
-        console.log(date.toLocaleString() + ': Connected to: ' + connector.endpoint.host);
-
-        // Show event
-        console.log(date.toLocaleString() + ': Subscribing to errors');
-
-        // Subscribe to errors channel
-        nem.com.websockets.subscribe.errors(connector, function (res) {
-            // Set time
-            date = new Date();
-            // Show event
-            console.log(date.toLocaleString() + ': Received error');
-            // Show data
-            console.log(date.toLocaleString() + ': ' + JSON.stringify(res));
-        });
-
-        // Show event
-        console.log(date.toLocaleString() + ': Subscribing to unconfirmed transactions of ' + connector.address);
-
-        // Subscribe to unconfirmed transactions channel
-        nem.com.websockets.subscribe.account.transactions.unconfirmed(connector, function (res) {
-            // Set time
-            date = new Date();
-            // Show event
-            console.log(date.toLocaleString() + ': Received unconfirmed transaction');
-            // Show data
-            console.log(date.toLocaleString() + ': ' + JSON.stringify(res));
-            ws.send(JSON.stringify({ unconfirmed: res }));
-        });
-
-        // Show event
-        console.log(date.toLocaleString() + ': Subscribing to confirmed transactions of ' + connector.address);
-
-        // Subscribe to confirmed transactions channel
-        nem.com.websockets.subscribe.account.transactions.confirmed(connector, function (res) {
-            // Set time
-            date = new Date();
-            // Show event
-            console.log(date.toLocaleString() + ': Received confirmed transaction');
-            // Show data
-            console.log(date.toLocaleString() + ': ' + JSON.stringify(res));
-        });
-
-    }, function (err) {
-        // Set time
-        date = new Date();
-        // Show event
-        console.log(date.toLocaleString() + ': An error occured');
-        // Show data
-        console.log(date.toLocaleString() + ': ' + JSON.stringify(err));
-        // Try to reconnect
-        reconnect();
-    });
-}
-
-function reconnect() {
-    // Replace endpoint object
-    endpoint = nem.model.objects.create("endpoint")("http://bob.nem.ninja", 7778);
-    // Replace connector
-    connector = nem.com.websockets.connector.create(endpoint, address);
-    // Set time
-    date = new Date();
-    // Show event
-    console.log(date.toLocaleString() + ': Trying to connect to: ' + endpoint.host);
-    // Try to establish a connection
-    connect(connector);
-}
